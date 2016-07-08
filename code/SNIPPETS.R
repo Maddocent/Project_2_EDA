805b72145af9fb58a3fa32a22801a07100bfdec1
Marc Teunis <marc.teunis@hu.nl>
2016-07-08 10:52:37 +0200

    changed title

--- code/SNIPPETS.R
+++ code/SNIPPETS.R
@@ -2,7 +2,7 @@
 
 # plotting only low values
 with(low, plot(low$year, low$Emissions, 
-               ylab = "Emissions(tons/year)", 
+               ylab = "Emissions in Baltimore (tons/year)", 
                xlab = "Year", main = "Low Emissions",
                cex.axis = 1.5, 
                cex.lab = 1.5, cex.main = 1.5))
